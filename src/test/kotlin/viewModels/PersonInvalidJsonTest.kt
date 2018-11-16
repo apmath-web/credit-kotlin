83bd5e443a4aae5b589fe9aae16f5df1b9756779
malinink <apmath@malinink.ru>
2018-11-17 00:49:34 +0300

    Add base person tests

---
+++ src/test/kotlin/viewModels/PersonInvalidJsonTest.kt
@@ -0,0 +1,30 @@
+package viewModels
+
+import org.json.JSONObject
+import org.junit.Assert.assertFalse
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class PersonInvalidJsonTest {
+
+    private fun invalidJsonProvider() = Stream.of(
+        Arguments.of("{\"lastName\":\"Malinin\"}"),
+        Arguments.of("{\"firstName\":null,\"lastName\":\"m\"}"),
+        Arguments.of("{\"firstName\":\"\",\"lastName\":\"m\"}")
+    )
+
+    @ParameterizedTest
+    @MethodSource("invalidJsonProvider")
+    fun validateTest(data: String) {
+        val person = Person()
+        val json = JSONObject(data)
+
+        assertFalse(person.loadAndValidate(json));
+        //TODO check concrete message given
+    }
+}
