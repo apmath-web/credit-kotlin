772b5692b7c7baa12abaac47262435319dffb852
malinink <apmath@malinink.ru>
2018-12-01 22:53:58 +0300

    Add hendler Exception handle functions, provide usefull json response functions in AbstractHandler

--- src/main/kotlin/actions/Handler.kt
+++ src/main/kotlin/actions/Handler.kt
@@ -5,37 +5,73 @@ import io.netty.handler.codec.http.*
 import domain.repositories.CreditsRepository
 import domain.repositories.CreditsRepositoryInterface
 import exceptions.ApiException
+import exceptions.BadRequestValidationException
 import exceptions.NotFoundException
+import org.json.JSONObject
 
-class Handler {
-    fun handle(request: HttpRequest): FullHttpResponse {
+
+class Handler : AbstractHandler() {
+
+    override fun handle(request: HttpRequest): FullHttpResponse {
+
         try {
             return routeRequest(request)
-        } catch (e : ApiException) {
-            return getResponse(e)
         } catch (e: Throwable) {
-            return getResponse(e);
+            return when (e) {
+                // order make sense
+                is ApiException -> getResponse(e)
+                is RuntimeException, is Error -> getUnexpectedExceptionResponse(e)
+                is Exception -> getExpectedExceptionResponse(e)
+                else -> getExpectedExceptionResponse(e)
+            }
         }
     }
 
     private fun routeRequest(request: HttpRequest): FullHttpResponse {
+
         when {
             request.uri() == "/credit" && request.method() == HttpMethod.POST
                 -> return Create(repository).handle(request)
 
         }
+
         throw NotFoundException("Route not found")
     }
 
     private fun getResponse(e: ApiException): FullHttpResponse {
-        return DefaultFullHttpResponse(HttpVersion.HTTP_1_1, e.status)
+
+        val json = JSONObject().put(MESSAGE, e.message)
+
+        if (e is BadRequestValidationException) {
+            val description = JSONObject()
+            e.messages.forEach {
+                description.put(it.field, it.text)
+            }
+            json.put(DESCRIPTION, description)
+        }
+
+        return getResponse(e.status, json)
     }
 
-    private fun getResponse(e: Throwable): FullHttpResponse {
-        return DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.INTERNAL_SERVER_ERROR)
+    private fun getUnexpectedExceptionResponse(e: Throwable): FullHttpResponse {
+
+        val json = JSONObject().put(MESSAGE, "Unexpected exception '${e.javaClass.toString()}' happend")
+
+        return getResponse(HttpResponseStatus.INTERNAL_SERVER_ERROR, json)
+    }
+
+    private fun getExpectedExceptionResponse(e: Throwable): FullHttpResponse {
+
+        val json = JSONObject().put(MESSAGE, "Expected exception '${e.javaClass.toString()}' not catched")
+
+        return getResponse(HttpResponseStatus.INTERNAL_SERVER_ERROR, json)
     }
 
     companion object {
+
         private val repository: CreditsRepositoryInterface = CreditsRepository()
+
+        const val MESSAGE       = "message"
+        const val DESCRIPTION   = "description"
     }
 }
