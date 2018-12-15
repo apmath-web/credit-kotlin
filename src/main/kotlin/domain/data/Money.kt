6f297e4b3159750de9b7cbb06f37f16be4f41d22
BarracudaPff <mrm.kikll@gmail.com>
2018-12-15 08:35:42 +0300

    upd money operators

--- src/main/kotlin/domain/data/Money.kt
+++ src/main/kotlin/domain/data/Money.kt
@@ -1,3 +1,24 @@
 package domain.data
 
-inline class Money(val value: Long)
+inline class Money(val value: Long) {
+    operator fun plus(money: Money): Money {
+        return Money(value + money.value)
+    }
+
+    operator fun times(money: Money): Money {
+        return Money(value * money.value)
+    }
+
+    operator fun div(money: Money): Money {
+        return Money(value / money.value)
+    }
+
+    operator fun minus(money: Money): Money {
+        return Money(value - money.value)
+    }
+
+    operator fun compareTo(money: Money): Int {
+        return value.compareTo(money.value)
+    }
+
+}
